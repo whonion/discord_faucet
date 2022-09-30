0c5e31ea386e185a1df5e313d870dfe5c3d261e2
RegEdit <47848969+whonion@users.noreply.github.com>
2022-09-30 14:54:38 +0800

    Update token_assmembler.sh

--- token_assmembler.sh
+++ token_assmembler.sh
@@ -1,4 +1,4 @@
 #create wallets
-for i in {1..161}; do celestia-appd keys add ML$i; done && \
+for i in {1..161}; do <tendermit-d> keys add ML$i; done && \
 #transfer of tokens to the main address
 for i in {1..161}; do <tendermit-d> tx bank send ML$i <your_address> 9960000utia --fees 500utia -y;done
